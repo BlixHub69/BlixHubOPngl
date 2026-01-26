84080d99c8023d709bd67a26efde571b84e2adb9
BlixHub69 <way2ramyadav@gmail.com>
2026-01-26 11:53:40 +0530

    Main-Hub-BlixHub.lua

--- Main-Hub-BlixHub.lua
+++ Main-Hub-BlixHub.lua
@@ -5,15 +5,13 @@ local Main = Window:NewTab("Blix Hub")
 local Section = Main:NewSection("Blix Hub")
 
 Section:NewButton("Speed Run Simulator", "Only works for Speed Run Simalator", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/BlixHub69/BlixHubOPngl/refs/heads/main/Script'))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlixHub69/BlixHubOPngl/refs/heads/main/Script"))()
 end)
 
 Section:NewButton("Arsenal", "Only works for Arsenal", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/VoidzyScripter/VoidHub-/main/Source%20Code'))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidzyScripter/VoidHub-/main/Source%20Code"))()
 end)
 
 Section:NewButton("Infinity Yield", "Infinity Yield", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua"))()
 end)
-
-end

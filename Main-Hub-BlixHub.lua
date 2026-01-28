3322955e926377b725b1ef389c107ad380b0b88e
BlixHub69 <way2ramyadav@gmail.com>
2026-01-28 17:11:58 +0530

    Main-Hub-BlixHub.lua

--- Main-Hub-BlixHub.lua
+++ Main-Hub-BlixHub.lua
@@ -12,6 +12,14 @@ Section:NewButton("Arsenal", "Only works for Arsenal", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlixHub69/BlixHubOPngl/refs/heads/main/Arsenal-BlixHub.lua"))()
 end)
 
+Section:NewButton("Murder Mystery 2", "Only works for Murder Mystery 2", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlixHub69/BlixHubOPngl/refs/heads/main/MM2-BlixHub.lua"))()
+end)
+
+Section:NewButton("KAT", "Only works for KAT", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlixHub69/BlixHubOPngl/refs/heads/main/KAT-BlixHub.lua"))()
+end)
+
 Section:NewButton("Infinity Yield", "Infinity Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua"))()
 end)

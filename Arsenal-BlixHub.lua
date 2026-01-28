d93c887a92960a1455f8ac71c860cdf393e8a89e
BlixHub69 <way2ramyadav@gmail.com>
2026-01-28 16:46:45 +0530

    Arsenal-BlixHub.lua

--- Arsenal-BlixHub.lua
+++ Arsenal-BlixHub.lua
@@ -15,24 +15,24 @@ Section:NewButton("Hitbox Expansion", "Idk i just copy pasted this shit", functi
     local players = getplrsname()
     local plr = game[players].LocalPlayer
     coroutine.resume(coroutine.create(function()
-    while  wait(1) do
+    while  wait(0.1) do
     coroutine.resume(coroutine.create(function()
     for _,v in pairs(game[players]:GetPlayers()) do
     if v.Name ~= plr.Name and v.Character then
     v.Character.RightUpperLeg.CanCollide = false
-    v.Character.RightUpperLeg.Transparency = 10
-    v.Character.RightUpperLeg.Size = Vector3.new(13,13,13)
+    v.Character.RightUpperLeg.Transparency = 1
+    v.Character.RightUpperLeg.Size = Vector3.new(1,1,1)
      
     v.Character.LeftUpperLeg.CanCollide = false
-    v.Character.LeftUpperLeg.Transparency = 10
-    v.Character.LeftUpperLeg.Size = Vector3.new(13,13,13)
+    v.Character.LeftUpperLeg.Transparency = 100
+    v.Character.LeftUpperLeg.Size = Vector3.new(1,1,1)
  
     v.Character.HeadHB.CanCollide = false
-    v.Character.HeadHB.Transparency = 10
+    v.Character.HeadHB.Transparency = 100
     v.Character.HeadHB.Size = Vector3.new(13,13,13)
      
     v.Character.HumanoidRootPart.CanCollide = false
-    v.Character.HumanoidRootPart.Transparency = 10
+    v.Character.HumanoidRootPart.Transparency = 100
     v.Character.HumanoidRootPart.Size = Vector3.new(13,13,13)
      
     end
